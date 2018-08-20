1a1753e6d2cfd496717e16da34372e761d82d087
Eva Lieu <itsevalieu@gmail.com>
2018-08-20 03:59:50 -0700

    Adding components to App container

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './components/Header/Header';
 import Bulletin from './components/Bulletin/Bulletin';
 import Article from './components/Article/Article';
 
@@ -9,7 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
         <Bulletin/>
         <Article/>
       </div>
