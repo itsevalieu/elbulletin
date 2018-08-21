6edb8decdf38a10b78b86640903573ae7b542b52
Eva Lieu <itsevalieu@gmail.com>
2018-08-20 23:40:22 -0700

    Adding article template

--- client/src/containers/App/components/Article/Article.jsx
+++ client/src/containers/App/components/Article/Article.jsx
@@ -5,7 +5,15 @@ class Article extends Component {
   render() {
     return (
       <div id='article' className='article'>
-      Article
+        <div id='details' className='article-details'>
+          <h1 id='title'></h1>
+          <p id='author'></p>
+          <p id='year'></p>
+          <p id='summary'></p>
+        </div>
+        <div id='body'>
+          <p id='text'></p>
+        </div>
       </div>
     );
   }
