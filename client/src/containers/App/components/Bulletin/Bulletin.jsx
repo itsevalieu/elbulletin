1659f82ede0653d8d2f08aa69bc9db5978595359
Eva Lieu <itsevalieu@gmail.com>
2018-08-20 22:28:52 -0700

    Adding sample posts; styling posts and hover corner shadow

--- client/src/containers/App/components/Bulletin/Bulletin.jsx
+++ client/src/containers/App/components/Bulletin/Bulletin.jsx
@@ -7,8 +7,47 @@ class Bulletin extends Component {
     return (
       <div id='bulletin' className='bulletin'>
         <Header/>
-        <div id='posts'>
-          
+        <div id='posts' className='posts'>
+          <section id='posts-creative'>
+            <div id='header' className='header'>
+              <div className='pushpin'></div>
+                <a href='/'>Creative Writing</a>
+              <div className='pushpin'></div>
+            </div>
+            <div className='post'>
+              <div className='pushpin'></div>
+              <p>The Monk</p><p>Short story about a father telling a story to his child about his days as a refugee living on an island.</p>
+            </div>
+            <div className='post'>Post</div>
+            <div className='post'>Post</div>
+          </section>
+          <section id='posts-literature'>
+            <div id='header' className='header'>
+              <div className='pushpin'></div>
+                <a href='/'>Literature Analysis</a>
+              <div className='pushpin'></div>
+            </div>
+            <div className='post'>Post</div>
+            <div className='post'>Post</div>
+          </section>
+          <section id='posts-research'>
+            <div id='header' className='header'>
+              <div className='pushpin'></div>
+                <a href='/'>Research Papers</a>
+              <div className='pushpin'></div>
+            </div>
+            <div className='post'>Post</div>
+            <div className='post'>Post</div>
+            <div className='post'>Post</div>
+          </section>
+          <section id='posts-technical'>
+            <div id='header' className='header'>
+              <div className='pushpin'></div>
+                <a href='/'>Technical Writing</a>
+              <div className='pushpin'></div>
+            </div>
+            <div className='post'>Post</div>
+          </section>
         </div>
       </div>
     );
