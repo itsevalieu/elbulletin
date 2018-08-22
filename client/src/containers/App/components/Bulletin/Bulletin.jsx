06b3795688dc22460a165c15a439720945919c1e
Eva Lieu <itsevalieu@gmail.com>
2018-08-22 01:16:48 -0700

    Removing hard coded posts, map function to replace

--- client/src/containers/App/components/Bulletin/Bulletin.jsx
+++ client/src/containers/App/components/Bulletin/Bulletin.jsx
@@ -1,111 +1,102 @@
 import React, { Component } from 'react';
+import {
+  Link,
+  Switch
+} from 'react-router-dom';
 import './Bulletin.css';
 import Header from '../Header/Header';
 
 class Bulletin extends Component {
-
-  handleClick(e) {
-    console.log(e.target.dataset.article);
+  constructor(props) {
+    super(props);
+    this.state = {
+      creative: this.props.posts.creative,
+      research: this.props.posts.research,
+      literature: this.props.posts.literature,
+      technical: this.props.posts.technical
+    }
   }
-
   render() {
     return (
-      <div id='bulletin' className='bulletin'>
-        <Header/>
-        <div id='posts' className='posts'>
-          <section id='posts-creative'>
-            <div id='header' className='header'>
-              <div className='pushpin'></div>
-                <a href='/'>Creative Writing</a>
-              <div className='pushpin'></div>
-            </div>
-            <div className='post' onClick={this.handleClick} data-article='themonk'>
-              <a href='/#article' data-article='themonk'>
+      <Switch>
+        <div id='bulletin' className='bulletin'>
+          <Header/>
+          <div id='posts' className='posts'>
+            <section id='posts-creative'>
+              <div id='header' className='header'>
                 <div className='pushpin'></div>
-                <p data-article='themonk'>The Monk</p>
-                <p data-article='themonk'>Short story about a father telling a story to his child about his days as a refugee living on an island.</p>
-              </a>
-            </div>
-            <div className='post'>
-              <a href='/#article'>
+                  <a href='/'>Creative Writing</a>
                 <div className='pushpin'></div>
-                <p>Plastic Ring</p>
-                <p>Short story about a father telling a story to his child about his days as a refugee living on an island.</p>
-              </a>
-            </div>
-            <div className='post'>
-              <a href='/#article'>
+              </div>
+              { 
+                this.state.creative.map((post, index) => (
+                  <Link key={index} to={post.path}>
+                    <div className='post' onClick={this.props.handleClick} data-article={post.path}>
+                        <div className='pushpin'></div>
+                        <p data-article={post.path}>{post.title}</p>
+                        <p data-article={post.path}>{post.summary}</p>
+                    </div>
+                  </Link>
+                ))
+              }
+            </section>
+            <section id='posts-literature'>
+              <div id='header' className='header'>
                 <div className='pushpin'></div>
-                <p>Countdown</p>
-                <p>Short story about a father telling a story to his child about his days as a refugee living on an island.</p>
-              </a>
-            </div>
-          </section>
-          <section id='posts-literature'>
-            <div id='header' className='header'>
-              <div className='pushpin'></div>
-                <a href='/#article'>Literature Analysis</a>
-              <div className='pushpin'></div>
-            </div>
-            <div className='post'>
-              <a href='/#article'>
+                  <a href='/#article'>Literature Analysis</a>
                 <div className='pushpin'></div>
-                <p>What's it going to be then, eh?</p>
-                <p>Short story about a father telling a story to his child about his days as a refugee living on an island.</p>
-              </a>
-            </div>
-            <div className='post'>
-              <a href='/#article'>
+              </div>
+              { 
+                this.state.literature.map((post, index) => (
+                  <Link key={index} to={post.path}>
+                    <div className='post' onClick={this.props.handleClick} data-article={post.path}>
+                        <div className='pushpin'></div>
+                        <p data-article={post.path}>{post.title}</p>
+                        <p data-article={post.path}>{post.summary}</p>
+                    </div>
+                  </Link>
+                ))
+              }
+            </section>
+            <section id='posts-research'>
+              <div id='header' className='header'>
                 <div className='pushpin'></div>
-                <p>Fight Club and Consumerism</p>
-                <p>Short story about a father telling a story to his child about his days as a refugee living on an island.</p>
-              </a>
-            </div>
-          </section>
-          <section id='posts-research'>
-            <div id='header' className='header'>
-              <div className='pushpin'></div>
-                <a href='/'>Research Papers</a>
-              <div className='pushpin'></div>
-            </div>
-            <div className='post'>
-              <a href='/#article'>
+                  <a href='/'>Research Papers</a>
                 <div className='pushpin'></div>
-                <p>Silence on the Los Angeles Front Lines</p>
-                <p>Immigrant Garment Workers and Rampant Wage Theft in Los Angeles</p>
-              </a>
-            </div>
-            <div className='post'>
-              <a href='/#article'>
+              </div>
+              { 
+                this.state.research.map((post, index) => (
+                  <Link key={index} to={post.path}>
+                    <div className='post' onClick={this.props.handleClick} data-article={post.path}>
+                        <div className='pushpin'></div>
+                        <p data-article={post.path}>{post.title}</p>
+                        <p data-article={post.path}>{post.summary}</p>
+                    </div>
+                  </Link>
+                ))
+              }
+            </section>
+            <section id='posts-technical'>
+              <div id='header' className='header'>
                 <div className='pushpin'></div>
-                <p>Justice for Janitors</p>
-                <p>Archival report on the Pasadena Justice for Janitors campaign.</p>
-              </a>
-            </div>
-            <div className='post'>
-              <a href='/#article'>
+                  <a href='/'>Web Development</a>
                 <div className='pushpin'></div>
-                <p>The Gig Economy</p>
-                <p>Uber, Lyft, and the changing economy surrounding independent contractors and tech companies.</p>
-              </a>
-            </div>
-          </section>
-          <section id='posts-technical'>
-            <div id='header' className='header'>
-              <div className='pushpin'></div>
-                <a href='/'>Technical Writing</a>
-              <div className='pushpin'></div>
-            </div>
-            <div className='post'>
-              <a href='/#article'>
-                <div className='pushpin'></div>
-                <p>Revel Cheat Sheet</p>
-                <p>Short story about a father telling a story to his child about his days as a refugee living on an island.</p>
-              </a>
-            </div>
-          </section>
+              </div>
+              { 
+                this.state.technical.map((post, index) => (
+                  <Link key={index} to={post.path}>
+                    <div className='post' onClick={this.props.handleClick} data-article={post.path}>
+                        <div className='pushpin'></div>
+                        <p data-article={post.path}>{post.title}</p>
+                        <p data-article={post.path}>{post.summary}</p>
+                    </div>
+                  </Link>
+                ))
+              }
+            </section>
+          </div>
         </div>
-      </div>
+      </Switch>
     );
   }
 }
