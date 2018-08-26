ee112e6c909440af9e8d3abaa548474466fb1792
Eva Lieu <itsevalieu@gmail.com>
2018-08-26 01:59:41 -0700

    Link to article id; display prop data

--- client/src/containers/App/components/Bulletin/components/Post/Post.jsx
+++ client/src/containers/App/components/Bulletin/components/Post/Post.jsx
@@ -7,11 +7,11 @@ import './Post.css';
 class Post extends Component {
   render() {
     return (
-        <Link key={this.props.index} to={this.props.post.path}>
-            <div className='post' onClick={this.props.handleClick} data-article={this.props.post.path}>
+        <Link to={`/api/articles/${this.props.post.article}`}>
+            <div className='post' onClick={this.props.handleClick}>
                 <div className='pushpin'></div>
-                <p data-article={this.props.post.path}>{this.props.post.title}</p>
-                <p data-article={this.props.post.path}>{this.props.post.summary}</p>
+                <p>{this.props.post.title}</p>
+                <p>{this.props.post.summary}</p>
             </div>
         </Link>
     );
