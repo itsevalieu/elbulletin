0142da4490433f9f63a52d7b6879f8bd59adb6cf
Eva Lieu <itsevalieu@gmail.com>
2018-08-26 21:29:19 -0700

    Pass article fetched from handleClick down as props from App parent

--- client/src/containers/App/components/Article/Article.jsx
+++ client/src/containers/App/components/Article/Article.jsx
@@ -3,56 +3,26 @@ import {
   Switch
 } from 'react-router-dom';
 import './Article.css';
-import axios from 'axios';
 
 class Article extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      articleId: this.props.articleId,
-      article: {}
-    }
-  }
-  componentWillReceiveProps(nextProps) {
-      console.log(nextProps);
-      console.log(this.state);
-      if(nextProps.value !== this.state) {
-        this.setState({
-          isLoaded: true,
-          articleId: this.props.articleId
-        });
-      }
-    }
-  componentDidMount() {
-    console.log('cats', this.state.articleId);
-    axios.get(`http://elbulletin-db.herokuapp.com/api/articles/`, {
-      params: {
-        id: this.state.articleId
-      }
-    }).then(({data}) => {
-      console.log('article data', data);
-      this.setState({
-        isLoaded: true,
-        article: data.data
-      });
-    }, (err) =>{
-      this.setState({
-        isLoaded: true,
-        err
-      });
-    });
   }
   render() {
+    if(this.props.article === null || this.props.article === undefined) return null;
+
     return (
       <Switch>
         <div id='article' className='article'>
           <div id='details' className='article-details'>
-            <h1 id='title'>{this.state.article.title}</h1>
-            <p id='author'>{this.state.article.author}</p>
-            <p id='year'>{this.state.article.year}</p>
-            <p id='summary'>{this.state.article.summary}</p>
+            <h1 id='title'>{this.props.article.title}</h1>
+            <p id='author'>{this.props.article.author}</p>
+            <p id='year'>{this.props.article.year}</p>
+            <p id='summary'>{this.props.article.summary}</p>
           </div>
-          <div id='body' className='article-body'>{this.state.article.body}</div>
+          <div id='body' className='article-body'> 
+            <p>{this.props.article.body}</p> 
+          </div>
         </div>
       </Switch>
     );
