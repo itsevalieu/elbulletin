6feffebe146fb3d4663aea0848b5d507b1dde4ad
Eva Lieu <itsevalieu@gmail.com>
2018-08-28 18:35:58 -0700

    Set up to deploy create-react-app on heroku with express

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //Router
 const articles = require('./server/routes/article.js');
@@ -30,7 +31,9 @@ const posts = require('./server/routes/post.js');
 
 app.use('/api/articles', articles);
 app.use('/api/posts', posts);
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 //Listener
 app.listen(PORT, function() {
   console.log('App listening on PORT ' + PORT);
