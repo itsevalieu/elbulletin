e880afb21c0a96360a09aa6ba84b300e2bf3ec68
Eva Lieu <itsevalieu@gmail.com>
2019-01-21 20:18:55 -0800

    Add social media icons, moved Posts into sections, added pushpins and paper, adjusted media queries

--- client/src/containers/App/components/Bulletin/Bulletin.jsx
+++ client/src/containers/App/components/Bulletin/Bulletin.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import {
-  Switch
+  Switch,
+  BrowserRouter as Router
 } from 'react-router-dom';
 import './Bulletin.css';
 import Header from './components/Header/Header';
 import Post from './components/Post/Post';
 import './components/Article/Article.css';
+import twitter from '../../../../assets/twitter.png';
+import github from '../../../../assets/github.png';
+import linkedin from '../../../../assets/linkedin.png';
 
 class Bulletin extends Component {
   shouldComponentUpdate(nextProps, nextState) {
@@ -24,42 +28,60 @@ class Bulletin extends Component {
     console.log('bulletin renders');
     console.log('article render');
     // if(this.props.articleId === '' || this.props.articleId === undefined) return null;
+    document.getElementsByClassName('tabs', function(e){
+      document
+      e.preventDefault();
+    });
     return (
       <Switch>
         <div id='bulletin' className='bulletin'>
-          {/* <Header name={'E.L Bulletin'}/> */}
-          <div>
-            <div className='tabs tab1'>Creative Writing</div>
-            <div className='tabs tab2'>Literature Analysis</div>
-            <div className='tabs tab3'>Research Papers</div>
-            <div className='tabs tab4'>Web Development</div>
-          </div>
+          <Router>
+            <div>
+              <div className='tabs tab1'><a href='/creative_writing'>Creative Writing</a></div>
+              <div className='tabs tab2'><a href='/literature_analysis'>Literature Analysis</a></div>
+              <div className='tabs tab3'><a href='/research_papers'>Research Papers</a></div>
+              <div className='tabs tab4'><a href='/web_development'>Web Development</a></div>
+            </div>
+          </Router>
           <div id='posts' className='posts'>
-            <section id='posts-creative'>
-              <Header name={'Creative Writing'}/>
-              { this.props.posts.creative.map((post, index) => ( <Post id='post' key={index} post={post} index={index} handleClick={this.props.handleClick}/> )) }
-            </section>
-            <section id='posts-literature'>
+            <section className='posts1'>
+              {/* <Header name={'Creative Writing'}/>
               <Header name={'Literature Analysis'}/>
+              <Header name={'Research Papers'}/>
+              <Header name={'Web Development'}/> */}
+              { this.props.posts.creative.map((post, index) => ( <Post key={index} post={post} index={index} handleClick={this.props.handleClick}/> )) }
+            </section>
+            <section className='posts2'>
               { this.props.posts.literature.map((post, index) => ( <Post key={index} post={post} index={index} handleClick={this.props.handleClick}/> )) }
             </section>
-            <section id='posts-research'>
-              <Header name={'Research Papers'}/>
+            <section className='posts3'>
               { this.props.posts.research.map((post, index) => ( <Post key={index} post={post} index={index} handleClick={this.props.handleClick}/> )) }
             </section>
-            <section id='posts-technical'>
-              <Header name={'Web Development'}/>
+            <section className='posts4'>
               { this.props.posts.technical.map((post, index) => ( <Post key={index} post={post} index={index} handleClick={this.props.handleClick}/> )) }
             </section>
           </div>
           <div id='article' className='article'>
-            <div id='details' className='article-details'>
-              <h1 id='title'>{this.props.article.title}</h1>
-              <p id='author'>{this.props.article.author}</p>
-              <p id='year'>{this.props.article.year}</p>
-              <p id='summary'>{this.props.article.summary}</p>
+            <div className='paper'>
+              <div className='pushpin paper-pushpin1'></div>
+              <div className='pushpin paper-pushpin2'></div>
+              <div className='pushpin paper-pushpin3'></div>
+              <div className='pushpin paper-pushpin4'></div>
+              <div id='details' className='article-details'>
+                <h2 id='title' className='paper-title'>{this.props.article.title}Title</h2>
+                <p id='author' className='paper-author'>{this.props.article.author}Author</p>
+                <p id='year' className='paper-year'>{this.props.article.year}Year</p>
+                <p id='summary' className='paper-summary'>{this.props.article.summary}Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quidem dolores recusandae at, voluptas qui harum pariatur, odit sint repellat cum tenetur nam, accusamus obcaecati impedit asperiores! Perspiciatis, doloremque ipsum.</p>
+              </div>
+              <iframe src={this.props.article.body} frameBorder="0" title='article'></iframe>
             </div>
-            <iframe src={this.props.article.body} frameBorder="0" title='article'></iframe>
+            <div className='pseudo-paper'></div>
+          </div>
+          <div className='social-media'>
+              <div><Header className='social-media-header' name={'Follow me:'}/></div>
+              <div className='sm'><a href='/' target='_blank'><img src={github} alt='github'/></a></div>
+              <div className='sm'><a href='/' target='_blank'><img src={linkedin} alt='linkedin'/></a></div>
+              <div className='sm'><a href='/' target='_blank'><img src={twitter} alt='twitter'/></a></div>
           </div>
         </div>
       </Switch>
