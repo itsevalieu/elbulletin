eae3998bd23b8d425d1c6d24a741f342bc5ecd8b
Eva Lieu <itsevalieu@gmail.com>
2019-02-19 13:00:33 -0800

    Fix links, animate icon hover

--- client/src/containers/App/components/Bulletin/components/Social/Social.jsx
+++ client/src/containers/App/components/Bulletin/components/Social/Social.jsx
@@ -10,9 +10,9 @@ import linkedin from './linkedin.png';
 function Social() {
     return (
         <div className='social'>
-            <Link to='https://github.com/itsevalieu' target='_blank'><img className='social__icon' src={github} alt='github'/></Link>
-            <Link to='https://www.linkedin.com/in/eva-lieu/' target='_blank'><img className='social__icon' src={linkedin} alt='linkedin'/></Link>
-            <Link to='https://twitter.com/itsevalieu' target='_blank'><img className='social__icon' src={twitter} alt='twitter'/></Link>
+            <a className='social__link' href='https://github.com/itsevalieu' target='_blank'><img className='social__icon' src={github} alt='github'/></a>
+            <a className='social__link' href='https://www.linkedin.com/in/eva-lieu/' target='_blank'><img className='social__icon' src={linkedin} alt='linkedin'/></a>
+            <a className='social__link' href='https://twitter.com/itsevalieu' target='_blank'><img className='social__icon' src={twitter} alt='twitter'/></a>
         </div>
     );
 }
